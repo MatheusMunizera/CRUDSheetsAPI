f16ac38827e17094b0a79a1b7ffe63f28901f79b
MatheusMunizera <matheus.munizera@gmail.com>
2022-10-09 19:04:25 -0300

    ui

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { SheetsController } from './controllers/sheets.controller';
 import { SheetsRepository } from './core/repositories/sheet.repository';
 import { SheetsCacheRepository } from './data/cache/sheets-cache/repository-cache.repository';
 import { SheetsService } from './services/sheets.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [SheetsController],
   providers: [
  {
